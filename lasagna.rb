23bb83e5c94cb7e1d213dee736232b7d2ec7141f
Tiago Correia <tco@keyholding.com>
2023-06-18 11:32:44 +0100

    Updated version

--- lasagna.rb
+++ lasagna.rb
@@ -1,11 +1,11 @@
 class Lasagna < Formula
-  @@version = "0.0.9"
+  @@version = "0.1.0"
   @@organisation = "TheKeyholdingCompany"
   @@app_name = "lasagna"
   desc "A CLI tool to build your lambda layer zip for you."
   homepage "https://github.com/#{@@organisation}/#{@@app_name}"
   url "https://github.com/#{@@organisation}/#{@@app_name}/archive/refs/tags/#{@@version}.tar.gz"
-  sha256 "b18abf7383acd12a95fde4bb12daa83e7c9a486eb3713202cabe99d65a14931d"
+  sha256 "d782f35d29d359ad8bfb02f5ba2ab35ed27575822029a4831c190bc5c4a14e00"
   license "Apache Licence, Version 2.0"
   head "https://github.com/#{@@organisation}/#{@@app_name}"
   
